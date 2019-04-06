7c7dc9cfdd252c46854b71a568fd3228d9ba7fd4
komarinko <keserapaseri8214@gmail.com>
2019-04-06 18:01:02 +0900

    [Finish] 2-7

--- length.rb
+++ length.rb
@@ -37,3 +37,20 @@ if apple <= 100
 	puts "りんごの値段は100円以下です" #false処理
 end
 #出力：りんごの値段は100円以上です
+
+tall = 180
+if tall >= 170 && tall <= 190
+	puts "身長は170以上190以下です。"
+end
+#出力：身長は170以上190以下です。
+
+
+apple =  "aomori"
+if apple == "Nagano"
+	puts "りんごの生産地は長野です"
+elsif apple == "Yamagata"
+	puts "りんごの生産地は山形です"
+else
+	puts "りんごの生産地は青森です"
+end
+
