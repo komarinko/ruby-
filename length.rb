57d16561cd84c87378a9f86888df8a27c27f80d9
komarinko <keserapaseri8214@gmail.com>
2019-04-06 17:14:29 +0900

    [Finish] 2-6

--- length.rb
+++ length.rb
@@ -12,3 +12,18 @@ puts 100
 puts 100 + 5
 puts "Rubyを学習"
 # 出力結果＝Rubyを学習
+name = "鈴木さん"
+puts "Hello" + name
+puts "こんにちは#{name}"
+# Hello鈴木さん
+# こんにちは鈴木さん
+names = ["Perl", "Ruby", "Python", "Swift"]
+puts names[1]
+
+tall = {"太郎"=>185, "二郎"=>170, "花子"=>150}
+puts tall["太郎"]
+#出力結果＝185
+
+tall = {:太郎=>185, :二郎=>170, :花子=>150}
+puts tall[:花子]
+#出力結果＝150
