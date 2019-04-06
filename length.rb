545cb0e1b03de38e64f06b4d19005fee27039ab9
komarinko <keserapaseri8214@gmail.com>
2019-04-06 23:41:26 +0900

    [Finish] 2-8

--- length.rb
+++ length.rb
@@ -60,3 +60,30 @@ a = gets.to_i
 b = gets.to_i
 puts "a+b=#{a+b}"
 #出力：a+b=5
+
+
+dice = 0
+while dice != 6 do
+	dice = rand(1..6)
+	puts dice
+end
+#ランダムでサイコロを振り、6になれば終了
+
+
+for i in 1..10 do
+	puts i
+end
+
+{"apple"=>130, "strawberry"=>180, "orange"=>100}.each do |fruit, price|
+	puts "#{fruit}は#{price}円です。"
+end
+
+
+i = 0
+while i <= 10 do
+	if i > 5
+		break #iが5より大きくなると繰り返しから抜ける
+	end
+	puts i
+	i += 1
+end
